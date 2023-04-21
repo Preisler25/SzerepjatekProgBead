b5096b1d39b7c2a19706746a9ed12871948fe4c7
Preisler <preisler.andras@gmail.com>
2023-04-21 17:48:10 +0200

    working shit

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ class StoryTeller:
         print(f"[ game ] : '{text}'")
 
     def ask(self, text, name):
-        return input(f"[ game ]: '{text}'\n[ {name} ]: ")
+        return input(f"[ game ] : '{text}'\n[ {name} ]: ")
 
 class Character:
-    def __init__(self, name, list_of_items, max_inv_len):
+    def __init__(self, name, list_of_items, max_inv_len, isAlive):
         self.name = name
         self.list_of_items = list_of_items
         self.max_inv_len = max_inv_len
+        self.isAlive = isAlive
 
     def stats(self):
         print(f"[ {self.name} ] : 'your items: {self.list_of_items} and your storage capacity: {self.max_inv_len}'")
@@ -22,23 +23,39 @@ class Dwarf:
         self.name = name
         self.list_of_questions = list_of_questions
 
-    def play(self, player_name):
-        question_aswered_correctly = False
+    def getQuestion(self, index):
+        for key in self.list_of_questions[index]:
+            print(key)
+            return key
+        
+    def getAnsw(self, index):
+        for key in self.list_of_questions[index]:
+            return self.list_of_questions[index][key]
+
+    def play(self, player):
         trys = 0
-        while (question_aswered_correctly == False or trys <4):
-            self.ask(self.list_of_questions[trys], player_name)
+        while (trys <3):
+            user_answ = self.ask(self.getQuestion(trys), player.name)
+            if user_answ == self.getAnsw(trys):
+                self.say("Helyes válasz! Nem hittem volna hogy ezt kitalálod :( Tessék itt az élelemed")
+                player.list_of_items.append("élelem")
+                return player
+            trys += 1
+        self.say("Sajnos nem válaszoltál helyesen! Ezért mint ahogy itt --> aláírtad felnégyellek")
+        player.isAlive = False
+        return player
 
     def say(self, text):
-        print(f"[ Dwarf {self.name} ]: '{text}'")
+        print(f"[ Dwarf {self.name} ] : '{text}'")
 
     def ask(self, text, name):
-        return input(f"[ Dwarf {self.name} ]: '{text}'\n[ {name} ]: ")
+        return input(f"[ Dwarf {self.name} ] : '{text}'\n[ {name} ]: ")
 
 
 def main():
     game = StoryTeller()
-    player = Character(game.ask("mi a neved?", "player"), [], 3)
-    dwarf0 = Dwarf("Erőske],", [{"kerdes1?", "valasz1"}, {"kerdes2?", "valasz2"}, {"kerdes3?", "valasz3"}])
-    game.say(f"Hello {player.name}!")
+    player = Character(game.ask("mi a neved?", "player"), [], 3, True)
+    dwarf0 = Dwarf("Erőske", [{"kerdes1?": "valasz1"}, {"kerdes2?": "valasz2"}, {"kerdes3?": "valasz3"}])
+    player = dwarf0.play(player)
 
 main()
